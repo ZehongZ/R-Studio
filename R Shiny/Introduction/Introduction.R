01e268d0095548ed7ab4d50592a12032e3902ff2
ZZHUANG <46541698+ZehongZ@users.noreply.github.com>
2019-04-18 02:11:24 -0500

    Add files via upload

---
+++ R Shiny/Introduction/Introduction.R
@@ -0,0 +1,19 @@
+#Example
+library(shiny)
+runExample("01_hello")
+
+#app.R has 3 components (a user interface object, a server function, a call to the shinyApp function)
+
+#ui object controls the layout and appearance of the app
+#server function contains the instructions that your computer needs to build your app
+#shinyApp function creates Shiny app objects from an explict UI/server pair
+
+
+
+runApp("Introduction")
+
+#Change the title from "Hello Shiny!" to "Hello World!"
+
+#Set the minimum value of the silder bar to 5
+
+#Change the histogram border color from whilte to orange
